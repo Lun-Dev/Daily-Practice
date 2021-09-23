250b86e4fe19f310d28cc8a105b8258be7732056
Lun-Dev <lunthedev@gmail.com>
2021-09-23 16:17:58 +0800

    Modified: Added comments to sumAll.js

---
+++ js_sumAll.js
@@ -0,0 +1,18 @@
+const sumAll = function(startingNum, endingNum) {
+    if (startingNum > 0 && // Number cannot be negative
+        endingNum > 0 &&  // Number cannot be negative
+        typeof startingNum === "number" && // Must be number data type
+        typeof endingNum === "number") { // Must be number data type
+    const newStartingNum = Math.min(startingNum, endingNum) // Find the lowest number between startingNum & endingNum
+    const newEndingNum = Math.max(startingNum, endingNum) // Find the highest number between startingNum & endingNum
+    let newTotal = 0 // A new total with a starting value of 0
+    for (let i = newStartingNum; i < newEndingNum +1; i++){
+        newTotal += i // Using newStartingNum as a start, ends with newEndingNum + 1; Adds to newTotal
+    }
+    return newTotal // Return the newTotal new value
+    } else {
+        return "ERROR" // If the numbers were negative or non-number, returns an error
+    }
+}
+
+console.log(sumAll(1,5))
