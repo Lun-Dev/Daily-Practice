91e1763ecf6a2a2e6ca49d95c9b060c21e66bc82
Lun-Dev <lunthedev@gmail.com>
2021-10-14 01:13:13 +0800

    Added: sum, multiply, power

--- 03_js_calculator/calculator.js
+++ 03_js_calculator/calculator.js
@@ -6,20 +6,25 @@ const subtract = function(a,b) {
 	return a - b;
 };
 
-const sum = function(a) {
-  a.reduce(getSum, 0);
-
-  function getSum(total, num) {
-    return total + num
-  }
+const sum = function(numbers) {
+  // let adding = 0;
+  // for (let i = 0; i < numbers.length; i++) {
+  //   adding += numbers[i];
+  // }
+  // return adding;
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  return numbers.reduce(reducer, 0); // 0 is the default
 };
 
-const multiply = function() {
 
+const multiply = function(numbers) {
+  const multiplier = (previousValue, currentValue) => previousValue * currentValue;
+  return numbers.reduce(multiplier);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const total = Math.pow(a,b);
+  return total;
 };
 
 const factorial = function() {
